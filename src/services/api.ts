4d3a637a4d50409e904809d8e20bbfd0d6835f26
Gustavo Lima <samtapes48@gmail.com>
2021-10-13 12:39:42 -0300

    Changes to heroky

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: process.env.NODE_ENV ? 'http://localhost:3333' : 'https://conncare-backend.herokuapp.com'
 })
 
 export default api
