12546ee6157ca827abdcb07f7f51eb2fcfe73e3b
Gustavo Lima <samtapes48@gmail.com>
2021-10-17 16:29:12 -0300

    finished consult meet html and css

--- src/pages/chamada/index.tsx
+++ src/pages/chamada/index.tsx
@@ -5,15 +5,28 @@ import Switch from 'react-switch'
 import './index.css'
 
 import {FaVideo, FaMicrophone, FaArrowRight} from 'react-icons/fa'
+import {BiSend, BiPaperclip} from 'react-icons/bi'
+import api from '../../services/api';
+import {useHistory} from 'react-router-dom'
+
+import Modal from 'react-bootstrap/Modal'
 
 export default function Chamada() {
-  const {consultMeet, user} = useContext(AuthContext)
+  const {consultMeet, user, removeConsultMeet} = useContext(AuthContext)
+  const history = useHistory()
 
   const [openCamera, setOpenCamera] = useState(false)
   const [openMic, setOpenMic] = useState(false)
+  const [message, setMessage] = useState('')
+  const [modalShow, setModalShow] = useState(false)
 
   function handleFinishConsult(){
-
+    api.put('/consult/' + consultMeet?.id + '/finish', {}, {headers: {'Authorization': user?.id}}).then((response) => {
+      removeConsultMeet();
+      history.push('/historico')
+    }).catch((err) => {
+      alert(err.response.data.message)
+    })
   }
 
   return (
@@ -36,7 +49,7 @@ export default function Chamada() {
               </div>
             </div>
 
-            <button onClick={handleFinishConsult} className="btn btn-finish-consult">Encerrar</button>
+            <button onClick={() => setModalShow(true)} className="btn btn-finish-consult">Encerrar</button>
           </div>
         </div>
 
@@ -50,16 +63,57 @@ export default function Chamada() {
           </div>
 
           <div className='chat-messages-container'>
+
             <div className='your-message'>
-              <p className='m-0'>Está me ouvindo?</p>
+              <div className='your-message-info'>
+                <p className='your-message-name'>Você</p>
+                <div className='your-message-image' style={{backgroundImage: 'url(' + user?.image_url + ')'}}/>
+              </div>
+              <p className='your-message-text'>Opa</p>
+            </div>
+
+            <div className='your-message'>
+              {/* <div className='your-message-info'>
+                <p className='your-message-name'>Você</p>
+                <div className='your-message-image' style={{backgroundImage: 'url(' + user?.image_url + ')'}}/>
+              </div> */}
+              <p className='your-message-text'>Está me ouvindo?</p>
             </div>
 
             <div className='other-message'>
-              <p>Sim.</p>
+              <div className='other-message-info'>
+                <div className='other-message-image' style={{backgroundImage: 'url(' + consultMeet?.image_url + ')'}}/>
+                <p className='other-message-name'>{consultMeet?.name}</p>
+              </div>
+              <p className='other-message-text'>Sim</p>
             </div>
+
+          </div>
+
+          <div className='send-message-container'>
+            <input className='send-message-input' placeholder='Digite aqui...' value={message} onChange={event => setMessage(event.target.value)}/>
+
+            <button className="btn p-0">
+              <BiPaperclip style={{transform: 'rotate(135deg)'}} color='#0166DA' size={28}/>
+            </button>
+
+            <button className="btn p-0">
+              <BiSend color='#0166DA' size={28}/>
+            </button>
           </div>
         </div>
       </div>
+
+      <Modal size='sm' centered show={modalShow} onHide={() => setModalShow(false)}>
+        <Modal.Body>
+          <p className='text-center mt-3'>Deseja mesmo encerrar a consulta?</p>
+            
+          <div className='modal-finish-consult-btns'>
+            <button onClick={() => setModalShow(false)} className="btn modal-btn-cancel-finish-consult">Ficar</button>
+            <button onClick={handleFinishConsult} className="btn btn-finish-consult">Sair</button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
